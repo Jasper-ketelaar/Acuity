be3176c9df2c382b5f186f1b3de8697e64377a01
Zach Herridge <zgherridge@gmail.com>
2018-05-29 14:00:51 -0500

    Secrets.

--- pathfinding_backend/src/main/java/com/acuitybotting/pathfinding/backend/services/tile_data/handler/aws/TileUploadHandler.java
+++ pathfinding_backend/src/main/java/com/acuitybotting/pathfinding/backend/services/tile_data/handler/aws/TileUploadHandler.java
@@ -1,12 +1,24 @@
 package com.acuitybotting.pathfinding.backend.services.tile_data.handler.aws;
 
 import com.acuitybotting.pathfinding.backend.services.tile_data.domain.TileUpload;
-import com.acuitybotting.pathfinding.backend.services.tile_data.utils.Constants;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.kms.AWSKMS;
+import com.amazonaws.services.kms.AWSKMSClientBuilder;
+import com.amazonaws.services.kms.model.DecryptRequest;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.secretsmanager.AWSSecretsManager;
+import com.amazonaws.services.secretsmanager.AWSSecretsManagerClientBuilder;
+import com.amazonaws.services.secretsmanager.model.GetSecretValueRequest;
+import com.amazonaws.services.secretsmanager.model.GetSecretValueResult;
+import com.amazonaws.services.secretsmanager.model.InvalidRequestException;
+import com.amazonaws.services.secretsmanager.model.ResourceNotFoundException;
+import com.amazonaws.util.Base64;
 import com.google.gson.Gson;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.security.InvalidParameterException;
 
 
 /**
@@ -19,6 +31,35 @@ public class TileUploadHandler implements RequestHandler<TileUpload, String> {
     @Override
     public String handleRequest(TileUpload tileUpload, Context context) {
         String json = gson.toJson(tileUpload);
-        return null;
+        return "Sec: " + decryptKey();
+    }
+
+    private static String decryptKey() {
+        String secretName = "MongoAcess";
+        String endpoint = "secretsmanager.us-east-1.amazonaws.com";
+        String region = "us-east-1";
+
+        AwsClientBuilder.EndpointConfiguration config = new AwsClientBuilder.EndpointConfiguration(endpoint, region);
+        AWSSecretsManagerClientBuilder clientBuilder = AWSSecretsManagerClientBuilder.standard();
+        clientBuilder.setEndpointConfiguration(config);
+        AWSSecretsManager client = clientBuilder.build();
+
+        GetSecretValueRequest getSecretValueRequest = new GetSecretValueRequest().withSecretId(secretName);
+        GetSecretValueResult getSecretValueResponse = null;
+        try {
+            getSecretValueResponse = client.getSecretValue(getSecretValueRequest);
+        } catch(ResourceNotFoundException e) {
+            System.out.println("The requested secret " + secretName + " was not found");
+        } catch (InvalidRequestException e) {
+            System.out.println("The request was invalid due to: " + e.getMessage());
+        } catch (InvalidParameterException e) {
+            System.out.println("The request had invalid params: " + e.getMessage());
+        }
+
+        if(getSecretValueResponse == null) {
+            return null;
+        }
+
+        return getSecretValueResponse.getSecretString();
     }
 }
