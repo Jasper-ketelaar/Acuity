7738ca90a13e90c0500e12ff87e7e0e9b1dfe7c5
Zach Herridge <zgherridge@gmail.com>
2018-05-30 16:01:44 -0500

    Fixed upload.

---
+++ pathfinding_backend/src/main/java/com/acuitybotting/pathfinding/backend/services/tile_data/handler/aws/TileUploadFunction.java
@@ -0,0 +1,52 @@
+package com.acuitybotting.pathfinding.backend.services.tile_data.handler.aws;
+
+import com.acuitybotting.db.arango.entities.TileFlagData;
+import com.acuitybotting.pathfinding.backend.services.tile_data.domain.TileCapture;
+import com.arangodb.springframework.core.ArangoOperations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.function.Function;
+
+/**
+ * Created by Zachary Herridge on 5/30/2018.
+ */
+@Component("tileUploadFunction")
+public class TileUploadFunction implements Function<TileCapture, String> {
+
+    @Autowired
+    private ArangoOperations arangoOperations;
+
+    @Override
+    public String apply(TileCapture tileCapture) {
+        int[][] map = tileCapture.getFlags();
+        Collection<TileFlagData> data = new HashSet<>();
+        for (int y = 0; y < map.length; y++) {
+            int[] flags = map[y];
+            for (int x = 0; x < flags.length; x++) {
+                int flag = flags[x];
+
+                int worldY = tileCapture.getY() + y;
+                int worldX = tileCapture.getX() + x;
+                int plane = tileCapture.getPlane();
+
+
+                TileFlagData build = TileFlagData.builder()
+                        .plane(plane)
+                        .location(new int[]{worldX, worldY})
+                        .key(worldX + "_" + worldY + "_" + plane)
+                        .flag(flag)
+                        .build();
+                data.add(build);
+            }
+        }
+        arangoOperations.upsert(data, ArangoOperations.UpsertStrategy.REPLACE);
+
+        tileCapture.getEntities().forEach(sceneEntity -> sceneEntity.setKey(sceneEntity.getLocation()[0] + "_" + sceneEntity.getLocation()[1] + "_" + sceneEntity.getPlane() + "_" + sceneEntity.getEntityID()));
+        arangoOperations.upsert(tileCapture.getEntities(), ArangoOperations.UpsertStrategy.REPLACE);
+        return "Success";
+    }
+}
--- pathfinding_backend/src/main/java/com/acuitybotting/pathfinding/backend/services/tile_data/handler/aws/TileUploadHandler.java
+++ pathfinding_backend/src/main/java/com/acuitybotting/pathfinding/backend/services/tile_data/handler/aws/TileUploadHandler.java
@@ -1,54 +1,13 @@
 package com.acuitybotting.pathfinding.backend.services.tile_data.handler.aws;
 
-import com.acuitybotting.db.arango.entities.TileFlagData;
 import com.acuitybotting.pathfinding.backend.services.tile_data.domain.TileCapture;
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.arangodb.springframework.core.ArangoOperations;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
-
-import java.util.Collection;
-import java.util.HashSet;
+import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
 
 
 /**
  * Created by Zachary Herridge on 5/29/2018.
  */
-@ComponentScan("com.acuitybotting.db.arango.repositories")
-public class TileUploadHandler implements RequestHandler<TileCapture, String> {
 
-    @Autowired
-    private ArangoOperations arangoOperations;
+public class TileUploadHandler extends SpringBootRequestHandler<TileCapture, String> {
 
-    @Override
-    public String handleRequest(TileCapture tileCapture, Context context) {
-        int[][] map = tileCapture.getFlags();
-        Collection<TileFlagData> data = new HashSet<>();
-        for (int y = 0; y < map.length; y++) {
-            int[] flags = map[y];
-            for (int x = 0; x < flags.length; x++) {
-                int flag = flags[x];
-
-                int worldY = tileCapture.getY() + y;
-                int worldX = tileCapture.getX() + x;
-                int plane = tileCapture.getPlane();
-
-
-                TileFlagData build = TileFlagData.builder()
-                        .plane(plane)
-                        .location(new int[]{worldX, worldY})
-                        .key(worldX + "_" + worldY + "_" + plane)
-                        .flag(flag)
-                        .requestID(context.getAwsRequestId())
-                        .build();
-                data.add(build);
-            }
-        }
-        arangoOperations.upsert(data, ArangoOperations.UpsertStrategy.REPLACE);
-
-        tileCapture.getEntities().forEach(sceneEntity -> sceneEntity.setKey(sceneEntity.getLocation()[0] + "_" + sceneEntity.getLocation()[1] + "_" + sceneEntity.getPlane() + "_" + sceneEntity.getEntityID()));
-        arangoOperations.upsert(tileCapture.getEntities(), ArangoOperations.UpsertStrategy.REPLACE);
-        return "Success";
-    }
 }
