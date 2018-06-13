78b1b9fd242b334810fb92acaf2b4fbcaf77506e
Zachary Herridge <zgherridge@gmail.com>
2018-06-12 23:31:15 -0500

    api setup.

--- service-script-repository/src/main/java/com/acuitybotting/script/repository/service/ScriptRepositoryService.java
+++ service-script-repository/src/main/java/com/acuitybotting/script/repository/service/ScriptRepositoryService.java
@@ -25,6 +25,38 @@ public class ScriptRepositoryService {
         this.obfuscatorService = obfuscatorService;
     }
 
+    public File downloadAndCompile(String repositoryName) throws Exception{
+        File parentFile = new File(UUID.randomUUID().toString().replaceAll("\\.", "-"));
+        if (!parentFile.mkdirs()) throw new RuntimeException("Failed to create temp file.");
+
+        File downloadLocation = new File(parentFile, repositoryName + ".zip");
+        gitHubService.downloadRepoAsZip(repositoryName, downloadLocation);
+
+        File unzipLocation = new File(parentFile, repositoryName);
+        compileService.unzip(downloadLocation, unzipLocation);
+
+        File outLocation = new File(parentFile, "out");
+        compileService.compile(
+                new File("C:\\Users\\zgher\\Documents\\RSPeer\\cache\\rspeer.jar"),
+                unzipLocation,
+                outLocation
+        );
+
+        File jarLocation = new File(parentFile,repositoryName + ".jar");
+        compileService.jar(outLocation, jarLocation);
+
+        File obfuscatedJarLocation = new File(parentFile,repositoryName + "-Obbed.jar");
+        obfuscatorService.obfuscate(
+                new File("C:\\Users\\zgher\\IdeaProjects\\Acuity\\service-script-repository\\src\\main\\resources\\allatori.jar"),
+                new File("C:\\Users\\zgher\\IdeaProjects\\Acuity\\service-script-repository\\src\\main\\resources\\config-placeholder.xml"),
+                new File(parentFile,"active.xml"),
+                jarLocation,
+                obfuscatedJarLocation
+        );
+
+        return parentFile;
+    }
+
     public String compileAndUpload(String repositoryName) throws Exception{
         File parentFile = new File(UUID.randomUUID().toString().replaceAll("\\.", "-"));
         if (!parentFile.mkdirs()) throw new RuntimeException("Failed to create temp file.");
