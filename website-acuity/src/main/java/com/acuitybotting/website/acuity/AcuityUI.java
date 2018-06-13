9cb0f9e2c9c5f53f388dcd142b819a2fb747196e
Zach Herridge <zgherridge@gmail.com>
2018-06-13 10:00:25 -0500

    Website view setup example.

--- website-acuity/src/main/java/com/acuitybotting/website/acuity/AcuityUI.java
+++ website-acuity/src/main/java/com/acuitybotting/website/acuity/AcuityUI.java
@@ -1,21 +1,49 @@
 package com.acuitybotting.website.acuity;
 
 import com.vaadin.annotations.Theme;
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewDisplay;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
+import com.vaadin.spring.annotation.SpringViewDisplay;
+import com.vaadin.ui.*;
+import com.vaadin.ui.themes.ValoTheme;
 
 /**
  * Created by Zachary Herridge on 6/13/2018.
  */
-@Theme("Valo")
+@Theme("valo")
 @SpringUI
-public class AcuityUI extends UI{
+@SpringViewDisplay
+public class AcuityUI extends UI implements ViewDisplay {
+
+    private Panel springViewDisplay;
 
     protected void init(VaadinRequest vaadinRequest) {
-        VerticalLayout mainLayout = new VerticalLayout(new Label("Hey my dudes"));
-        setContent(mainLayout);
+        final VerticalLayout root = new VerticalLayout();
+        root.setSizeFull();
+        setContent(root);
+
+        final CssLayout navigationBar = new CssLayout();
+        navigationBar.addStyleName(ValoTheme.LAYOUT_COMPONENT_GROUP);
+        navigationBar.addComponent(createNavigationButton("View Scoped View", "TestView"));
+        root.addComponent(navigationBar);
+
+        springViewDisplay = new Panel();
+        springViewDisplay.setSizeFull();
+        root.addComponent(springViewDisplay);
+        root.setExpandRatio(springViewDisplay, 1.0f);
+    }
+
+    private Button createNavigationButton(String caption, final String viewName) {
+        Button button = new Button(caption);
+        button.addStyleName(ValoTheme.BUTTON_SMALL);
+        button.addClickListener(event -> getUI().getNavigator().navigateTo(viewName));
+        return button;
+    }
+
+    @Override
+    public void showView(View view) {
+        springViewDisplay.setContent((Component) view);
     }
 }
---
+++ website-acuity/src/main/java/com/acuitybotting/website/acuity/TestView.java
@@ -0,0 +1,23 @@
+package com.acuitybotting.website.acuity;
+
+import com.vaadin.navigator.View;
+import com.vaadin.spring.annotation.SpringView;
+import com.vaadin.spring.annotation.UIScope;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.VerticalLayout;
+
+import javax.annotation.PostConstruct;
+
+
+/**
+ * Created by Zachary Herridge on 6/13/2018.
+ */
+@UIScope
+@SpringView(name = "TestView")
+public class TestView extends VerticalLayout implements View{
+
+    @PostConstruct
+    void init(){
+        addComponent(new Label("This is the default view"));
+    }
+}
